c8ccec6b4f888a5513d46790cd4c68c424ef27dc
Pavel Tikhonov <83917023+Fuithias@users.noreply.github.com>
2024-04-01 12:21:13 +0300

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -2,6 +2,6 @@ from Bot.logging import logger  # Importing the logger function from Bot.logging
 from Bot import bot  # Importing the bot instance from the Bot module
 
 # Getting the logger for the current module and logging an informational message
-logger(__name__).info('Client successfully initiated....')
+logger('Bot').info('Client successfully initiated....')
 # Running the bot
 bot.run()
